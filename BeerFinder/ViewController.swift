d9b82911f3ed35bfe8d7bacb1f462f5039b7f71f
Alexander Tsvetanov <alexandervltsvetanov@gmail.com>
2020-07-31 16:13:58 +0300

    First commit

---
+++ BeerFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeerFinder
+//
+//  Created by Sasha Belov on 31.07.20.
+//  Copyright © 2020 Alexander Tsvetanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
